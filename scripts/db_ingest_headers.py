c784a4b832bca6811c184ac4fbe95a0eb4115b8b
Andy Casey <andycasey@gmail.com>
2016-12-15 15:18:47 +0000

    Chunk out the ingestion to 10 parallel connections

--- scripts/db_ingest_headers.py
+++ scripts/db_ingest_headers.py
@@ -16,7 +16,7 @@ from astropy.io import fits
 cwd = os.path.dirname(__file__)
 
 DEBUG = False
-THREADS = 20
+THREADS = 10
 DATA_DIR = os.path.realpath(os.path.join(cwd, "../data/spectra/"))
 
 # Database credentials
@@ -32,12 +32,12 @@ with open(os.path.join(cwd, "../db/obs-columns.yaml"), "r") as fp:
     columns = yaml.load(fp)
 
 
-def _ingest_obs_headers(filename):
+def _ingest_obs_headers(filenames):
     """
     Ingest the headers from a reduced HARPS spectrum (*_bis_G2_A.fits product).
 
-    :param filename:
-        The local path of the reduced FITS file.
+    :param filenames:
+        The local path of the reduced FITS files.
     """
 
     connection = pg.connect(**credentials)
@@ -45,86 +45,93 @@ def _ingest_obs_headers(filename):
 
     required_keys = ("drs_ccf_rvc", "mjd_obs", "drs_dvrms")
 
-    try:
-        with fits.open(filename) as image:
-            cursor.execute(
-                """SELECT EXISTS(SELECT 1 FROM obs WHERE date_obs=%s)""",
-                (image[0].header["DATE-OBS"], ))
-            exists, = cursor.fetchone()
-            
-            if not exists:
+    for filename in filenames:
+        cursor = connection.cursor()
 
-                keys = []
-                values = []
-                for header_key, sql_key in columns.items():
-                    try:
-                        value = image[0].header.get(header_key, None)
-
-                    except fits.VerifyError:
-                        print("Couldn't parse key '{}' from {}".format(
-                            header_key, filename))
-
-                        if sql_key == "date_obs":
-                            # If this is the case, we are just fucked.
-                            raise
-
-                    else:
-                        keys.append(sql_key)
-
-                        # Some 2004 data fail because they record NaNs like this:
-                        if value == "NaN." \
-                        or (sql_key in required_keys and value is None):
-                            value = "NaN"
-
-                        values.append(value)
-
-                # Sometimes the DATE-OBS and filename can be a millisecond different
-                # from each other, so we are storing the filename stub here too.
-                keys += ["filename"]
-                values += ["/".join(filename.split("/")[-2:])]
+        try:
+            with fits.open(filename) as image:
+                cursor.execute(
+                    """SELECT EXISTS(SELECT 1 FROM obs WHERE date_obs=%s)""",
+                    (image[0].header["DATE-OBS"], ))
+                exists, = cursor.fetchone()
                 
-                for required_key in required_keys:
-                    if required_key not in keys:
-                        keys.append(required_key)
-                        values.append(np.nan)
-                        print("ADDED FOR ", required_key)
+                if not exists:
 
-                # Prepare inputs for the request.
-                fillers = ", ".join(["%s"] * len(keys))
-                column_names = ", ".join(keys)
-                
-                try:
-                    cursor.execute(
-                        """INSERT INTO obs ({column_names}) VALUES ({fillers})"""\
-                        .format(column_names=column_names, fillers=fillers), 
-                        values)
+                    keys = []
+                    values = []
+                    for header_key, sql_key in columns.items():
+                        try:
+                            value = image[0].header.get(header_key, None)
 
-                except (pg.IntegrityError, pg.DataError):
-                    logging.exception("Ingestion error on {}:".format(filename))
-                    connection.rollback()
-                    if DEBUG: raise
+                        except fits.VerifyError:
+                            print("Couldn't parse key '{}' from {}".format(
+                                header_key, filename))
+
+                            if sql_key == "date_obs":
+                                # If this is the case, we are just fucked.
+                                continue
+
+                        else:
+                            keys.append(sql_key)
+
+                            # Some 2004 data fail because they record NaNs like this:
+                            if value == "NaN." \
+                            or (sql_key in required_keys and value is None):
+                                value = "NaN"
+
+                            values.append(value)
+
+                    # Sometimes the DATE-OBS and filename can be a millisecond different
+                    # from each other, so we are storing the filename stub here too.
+                    keys += ["filename"]
+                    values += ["/".join(filename.split("/")[-2:])]
                     
-                else:
-                    connection.commit()
+                    for required_key in required_keys:
+                        if required_key not in keys:
+                            keys.append(required_key)
+                            values.append(np.nan)
+                            print("ADDED FOR ", required_key)
 
-    except IOError:
-        logging.exception("IOError while opening {}".format(filename))
+                    # Prepare inputs for the request.
+                    fillers = ", ".join(["%s"] * len(keys))
+                    column_names = ", ".join(keys)
+                    
+                    try:
+                        cursor.execute(
+                            """INSERT INTO obs ({column_names}) VALUES ({fillers})"""\
+                            .format(column_names=column_names, fillers=fillers), 
+                            values)
 
-    cursor.close()
+                    except (pg.IntegrityError, pg.DataError):
+                        logging.exception("Ingestion error on {}:".format(filename))
+                        connection.rollback()
+                        if DEBUG: raise
+                        
+                    else:
+                        connection.commit()
+
+        except IOError:
+            logging.exception("IOError while opening {}".format(filename))
+
+        cursor.close()
+
     connection.close()
 
     return None
 
 
-# Failure on at least:
-#/media/My Book/research/orchestra/data/spectra/data/reduced/2004-10-05/HARPS.2004-10-05T03:16:30.268_bis_G2_A.fits:
-
-
-
 # Ingest the headers from each file.
-print("There are {} files to check (and potentially ingest)".format(N))
+print("Opening {} threads to ingest {} files".format(THREADS, N))
 
+
+# Chunk it out.
+results = []
 pool = mp.Pool(THREADS)
-result = pool.map_async(_ingest_obs_headers, obs_filenames).get()
+s = int(np.ceil(float(N)/THREADS))
+for t in range(THREADS):
+    results.append(
+        pool.map_async(_ingest_obs_headers, obs_filenames[t * s:(t + 1) * s]))
+
+r = [result.get() for result in results]
 pool.close()
 pool.join()
