701befcb7cd68e37ddd377648d3f51f05d3a239d
Andy Casey <andycasey@gmail.com>
2016-12-16 13:41:49 +0000

    Measure stellar activity in parallel

--- scripts/measure_shk.py
+++ scripts/measure_shk.py
@@ -6,6 +6,7 @@ Measure the S_HK statistic as a proxy for stellar activity.
 import os
 import logging
 import numpy as np
+import multiprocessing as mp
 import psycopg2 as pg
 import yaml
 from astropy.io import fits
@@ -24,7 +25,7 @@ logger.addHandler(hdl)
 # Set some configuration values.
 cwd = os.path.dirname(__file__)
 OVERWRITE = True
-THREADS = 8
+THREADS = 10
 DATA_DIR = os.path.join(cwd, "../data/spectra/")
 
 # Find all spectra.
@@ -37,7 +38,7 @@ with open(os.path.join(cwd, "../db/credentials.yaml"), "r") as fp:
     credentials = yaml.load(fp)
 
 # Create a function to process all spectra in parallel.
-def _process_stellar_activity(filename):
+def _process_stellar_activity(filename, connection):
     """
     Measure the S_HK index as a proxy of stellar activity from a reduced HARPS
     spectrum, and ingest that measurement into the database.
@@ -58,27 +59,8 @@ def _process_stellar_activity(filename):
         wavelength = np.arange(naxis) * cdelt + crval
         flux = image[0].data
 
-    connection = pg.connect(**credentials)
-    cursor = connection.cursor()
-
-    def _cleanup():
-        cursor.close()
-        connection.close()
-        return None
-
-    # Check to see if we have already measured the activity from this spectrum.
-    cursor.execute(
-        """SELECT EXISTS(SELECT 1 FROM stellar_activity WHERE date_obs=%s)""",
-        (date_obs, ))
-    exists, = cursor.fetchone()
-
-    if exists and not OVERWRITE:
-        logger.info(
-            "Activity already measured for date_obs = '{}'; not overwriting"\
-            .format(date_obs))
-        return _cleanup()
-
     # Get the measured radial velocity from the database.
+    cursor = connection.cursor()
     cursor.execute(
         """ SELECT drs_ccf_rvc AS stellar_rv
             FROM obs WHERE date_obs = %s""", (date_obs, ))
@@ -86,7 +68,8 @@ def _process_stellar_activity(filename):
         logger.warn(
             "No headers ingested for observation with date_obs = '{}'"\
             .format(date_obs))
-        return _cleanup()
+        cursor.close()
+        return None
     
     rv, = cursor.fetchone()
     rv = float(rv)
@@ -105,33 +88,44 @@ def _process_stellar_activity(filename):
             .format(s_hk, e_s_hk, object_name, basename, date_obs))
 
     # Insert or update this measurement in the database.
-    if exists:
-        cursor.execute(
-            """ UPDATE stellar_activity
-                SET s_hk = %s,
-                    e_s_hk = %s,
-                    filename = %s
-                WHERE date_obs = %s """,
-            (s_hk, e_s_hk, basename, date_obs))
-
-    else:
+    try:
         cursor.execute(
             """ INSERT INTO stellar_activity (date_obs, filename, s_hk, e_s_hk)
-                VALUES (%s, %s, %s, %s)""",
+                VALUES (%s, %s, %s, %s)
+                ON CONFLICT DO NOTHING;""",
             (date_obs, basename, s_hk, e_s_hk))
 
+    except (pg.IntegrityError, pg.DataError):
+        logger.warn("Exception when ingesting {}".format(filename))
+        connection.rollback()
+        
+    else:
+        connection.commit()
+
+    cursor.close()
+    return None
+
+
+def process_stellar_activity_in_parallel(*filenames):
+    connection = pg.connect(**credentials)
+    for filename in filenames:
+        _process_stellar_activity(filename, connection)
+
     connection.commit()
-
-    return _cleanup()
-    
-
-for filename in filenames:
-    _process_stellar_activity(filename)
+    connection.close()
 
 
-raise a
-
+# Chunk it out.
 pool = mp.Pool(THREADS)
-result = pool.map_async(_process_stellar_activity, filenames).get()
-pool.close()
+s = int(np.ceil(float(N)/THREADS))
+
+results = []
+for t in range(THREADS):
+    results.append(
+        pool.apply_async(
+            process_stellar_activity_in_parallel, filenames[t * s:(t + 1) * s]))
+
+results = [each.get() for each in results]
 pool.join()
+pool.close()
+
